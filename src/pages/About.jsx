5019bd301a4e0f2b837fe3d447c194e9912614d8
ratmans <ratma96@gmail.com>
2023-11-11 22:02:12 +0000

    second commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,6 +6,12 @@ const About = () => {
       <div className="mt-5 flex flex-col gap-3 text-slate-500">
         <p >Full-stack developer based in Senegal, specialized in the field of ICT (Information and Communication Technology), while constantly seeking knowledge and keeping up with the latest advancements. </p>
       </div>
+    <div className="py-10 flex flex-col">
+      <h3 className="subhead-text">My Skills</h3>
+      <div className="mt-16 flex flex-wrap gap-12">
+
+      </div>
+    </div>
     </section>
   )
 }
